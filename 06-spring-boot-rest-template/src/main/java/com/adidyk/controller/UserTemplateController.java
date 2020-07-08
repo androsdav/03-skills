4d123960676bc6ffb3c14862ae8e674703ce3d4f
androsdav <androsdav@gmail.com>
2020-07-08 17:46:40 +0300

    adds new example rest template

--- 06-spring-boot-rest-template/src/main/java/com/adidyk/controller/UserTemplateController.java
+++ 06-spring-boot-rest-template/src/main/java/com/adidyk/controller/UserTemplateController.java
@@ -6,10 +6,7 @@ import org.springframework.http.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -37,10 +34,12 @@ public class UserTemplateController {
     }
 
     /**
+     * ---- GET -----
      * findAllUser - returns all user in string format.
+     * getForObject - get for object -> string format.
      * @return - returns all user in string format.
      */
-    @RequestMapping(value = "/template/find_all_user/string_format")
+    @RequestMapping(value = "/template/find_all_user/get_for_object/string")
     public String findAllUserStringFormat() {
         String result = this.restTemplate.getForObject("http://localhost:8081/find_all_user", String.class);
         System.out.println("String result: " + result);
@@ -48,28 +47,54 @@ public class UserTemplateController {
     }
 
     /**
+     * ---- GET -----
      * findAllUser - returns all user in string format.
-     * @return - returns all user in string format.
+     * getForObject - get for object -> user format.
+     * @return - returns all user in json format.
      */
-    @RequestMapping(value = "/template/find_all_user/object_format")
+    @RequestMapping(value = "/template/find_all_user/get_for_object/object")
     public List<User> findAllUserList() {
        User[] users = this.restTemplate.getForObject("http://localhost:8081/find_all_user", User[].class);
-        return Arrays.asList(Objects.requireNonNull(users));
+       return Arrays.asList(Objects.requireNonNull(users));
     }
 
-    /*
-    @RequestMapping(value = "/template/find_all_user/json_format")
-    public List<User> findAllUserJSONFormat() {
+    /**
+     * ---- GET -----
+     * findAllUser - returns all user in string format.
+     * getForObject - get for object -> user format.
+     * @return - returns all user in json format.
+     */
+    @RequestMapping(value = "/template/find_all_user/get_for_object/response")
+    public ResponseEntity<List<User>> findAllUserResponseEntity() {
+        User[] users = this.restTemplate.getForObject("http://localhost:8081/find_all_user", User[].class);
+        return new ResponseEntity<>(Arrays.asList(Objects.requireNonNull(users)), HttpStatus.OK);
+    }
 
+    /**
+     * ---- GET -----
+     * findAllUser - returns all user in string format.
+     * getForObject - get for object -> entity.
+     * @return - returns all user in json format.
+     */
+    @RequestMapping(value = "/template/find_all_user/get_for_entity/string")
+    public ResponseEntity<String> findAllUserEntity() {
+        ResponseEntity<String> responseEntity = this.restTemplate.getForEntity("http://localhost:8081/find_all_user", String.class);
+        return responseEntity;
+    }
+
+    /**
+     * ---- GET -----
+     * findAllUserExchange - find all user exchange.
+     * exchange - support request headers.
+     * @return - return string format.
+     */
+    @RequestMapping(value = "/template/find_all_user/json_format")
+    public ResponseEntity<String> findAllUserExchange() {
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        // HttpEntity<String>: To get result as String.
         HttpEntity<String> entity = new HttpEntity<>(headers);
-
-        String response = this.restTemplate.exchange("http://localhost:8081/find_all_user", HttpMethod.GET, entity, String.class).getBody();
-
+        ResponseEntity<String> response = this.restTemplate.exchange("http://localhost:8081/find_all_user", HttpMethod.GET, entity, String.class);
         return response;
     }
-    */
 
 }
