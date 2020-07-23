e70e3374bec143ea6a09d6474ac9390c9a7515fc
androsdav <androsdav@gmail.com>
2020-07-23 18:12:12 +0300

    adds mongoDB shell in idea

--- 08-spring-boot-mongoDB/src/main/java/com/adidyk/StartUi.java
+++ 08-spring-boot-mongoDB/src/main/java/com/adidyk/StartUi.java
@@ -1,7 +1,5 @@
 package com.adidyk;
 
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
