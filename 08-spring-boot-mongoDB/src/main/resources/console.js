4da7aef405cec835a97fe9338393be396d7352ff
androsdav <androsdav@gmail.com>
2020-07-24 17:21:59 +0300

    adds mongoDB shell in idea

--- 08-spring-boot-mongoDB/src/main/resources/console.js
+++ 08-spring-boot-mongoDB/src/main/resources/console.js
@@ -1,3 +1,7 @@
+/*  */
+use users
+
+
 db.users.insertMany([
     {"name": "Alice", "age": 31, languages: ["english", "french"]},
     {"name": "Lene", "age": 29, languages: ["english", "spanish"]},
