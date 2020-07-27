7e5c5e4da01b7b2749c01426b026f0819f6f02d4
androsdav <androsdav@gmail.com>
2020-07-27 18:19:06 +0300

    adds find / sort / limit / skip mongoDB

--- 08-spring-boot-mongoDB/src/main/resources/console.js
+++
@@ -1,9 +0,0 @@
-/*  */
-use users
-
-
-db.users.insertMany([
-    {"name": "Alice", "age": 31, languages: ["english", "french"]},
-    {"name": "Lene", "age": 29, languages: ["english", "spanish"]},
-    {"name": "Kate", "age": 30, languages: ["german", "russian"]}
-    ]);
---
+++ 08-spring-boot-mongoDB/src/main/resources/users.js
@@ -0,0 +1,20 @@
+db.users.insert([
+    {
+        "name" : "Linda",
+        "age" : 18,
+        "pet" : {
+            "type" : "cat",
+            "name" : "nina"
+        },
+        "languages" : ["english"]
+    },
+    {
+        "name" : "Lisa",
+        "age" : 23,
+        "pet" : {
+            "type" : "parrot",
+            "name" : "corsair"
+        },
+        "languages" : ["german"]
+    }
+])
